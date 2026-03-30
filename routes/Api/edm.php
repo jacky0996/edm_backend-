ac1e1d72937ea34117a79cde5b38db7bf7fdf1fe
shanelin1 <shane.lin@hwacom.com>
2026-03-30 13:53:23 +0800

    調整route寫法

--- routes/Api/edm.php
+++ routes/Api/edm.php
@@ -1,38 +1,44 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EDM\MemberController;
+use App\Http\Controllers\EDM\GroupController;
+use App\Http\Controllers\EDM\EventController;
+use App\Http\Controllers\EDM\SSOController;
 
 Route::prefix('edm')->group(function () {
     Route::prefix('member')->group(function () {
-        Route::post('/list', 'EDM\MemberController@list');
-        Route::post('/view', 'EDM\MemberController@view');
-        Route::post('/add', 'EDM\MemberController@add');
-        Route::post('/editStatus', 'EDM\MemberController@editStatus');
-        Route::post('/editEmail', 'EDM\MemberController@editEmail');
-        Route::post('/editMobile', 'EDM\MemberController@editMobile');
-        Route::post('/editSales', 'EDM\MemberController@editSales');
+        Route::post('/list', [MemberController::class, 'list']);
+        Route::post('/view', [MemberController::class, 'view']);
+        Route::post('/add', [MemberController::class, 'add']);
+        Route::post('/editStatus', [MemberController::class, 'editStatus']);
+        Route::post('/editEmail', [MemberController::class, 'editEmail']);
+        Route::post('/editMobile', [MemberController::class, 'editMobile']);
+        Route::post('/editSales', [MemberController::class, 'editSales']);
     });
 
     Route::prefix('group')->group(function () {
-        Route::post('/list', 'EDM\GroupController@list');
-        Route::post('/view', 'EDM\GroupController@view');
-        Route::post('/editStatus', 'EDM\GroupController@editStatus');
-        Route::post('/create', 'EDM\GroupController@create');
-        Route::post('/getEventList', 'EDM\GroupController@getEventList');
+        Route::post('/list', [GroupController::class, 'list']);
+        Route::post('/view', [GroupController::class, 'view']);
+        Route::post('/editStatus', [GroupController::class, 'editStatus']);
+        Route::post('/create', [GroupController::class, 'create']);
+        Route::post('/getEventList', [GroupController::class, 'getEventList']);
     });
+
     Route::prefix('event')->group(function () {
-        Route::post('/list', 'EDM\EventController@list');
-        Route::post('/view', 'EDM\EventController@view');
-        Route::post('/create', 'EDM\EventController@create');
-        Route::post('/update', 'EDM\EventController@update');
-        Route::post('/editStatus', 'EDM\EventController@editStatus');
-        Route::post('/imageUpload', 'EDM\EventController@imageUpload');
-        Route::post('/getImage', 'EDM\EventController@getImage');
-        Route::post('/getInviteList', 'EDM\EventController@getInviteList');
-        Route::post('/importGroup', 'EDM\EventController@importGroup');
+        Route::post('/list', [EventController::class, 'list']);
+        Route::post('/view', [EventController::class, 'view']);
+        Route::post('/create', [EventController::class, 'create']);
+        Route::post('/update', [EventController::class, 'update']);
+        Route::post('/editStatus', [EventController::class, 'editStatus']);
+        Route::post('/imageUpload', [EventController::class, 'imageUpload']);
+        Route::post('/getImage', [EventController::class, 'getImage']);
+        Route::post('/getInviteList', [EventController::class, 'getInviteList']);
+        Route::post('/importGroup', [EventController::class, 'importGroup']);
     });
+
     /** SSO 驗證 API (掛載 IP 白名單防護) */
     Route::middleware([\App\Http\Middleware\WhitelistIpMiddleware::class])->group(function () {
-        Route::post('/sso/verify-token', 'EDM\SSOController@verifyToken')->name('api.edm.sso.verify');
+        Route::post('/sso/verify-token', [SSOController::class, 'verifyToken'])->name('api.edm.sso.verify');
     });
 });
